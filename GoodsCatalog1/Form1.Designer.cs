4afc1381d3548a90e1c134afffeb2259f3f0d368
MickleGoncharuk <enzo32@yandex.ru>
2017-03-05 17:01:09 +0200

    update

---
+++ GoodsCatalog1/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace GoodsCatalog1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Категории:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 566);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Каталог";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 518);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Добавить в корзину";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(10, 195);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(194, 308);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить категорию";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(231, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(209, 566);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Корзина";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 518);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 474);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Удалить из корзины";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(34, 426);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 32);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ОБщая стоимость";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 19;
+            this.listBox2.Location = new System.Drawing.Point(6, 157);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(197, 232);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GoodsCatalog1.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.Location = new System.Drawing.Point(446, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(328, 289);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Просмотр";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GoodsCatalog1.Properties.Resources.logo2;
+            this.pictureBox2.Location = new System.Drawing.Point(7, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(315, 257);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Controls.Add(this.button6);
+            this.groupBox4.Controls.Add(this.textBox5);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.textBox4);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.textBox3);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Location = new System.Drawing.Point(453, 309);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(315, 270);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Редактирование";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(9, 222);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(299, 29);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Удалить товар";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(10, 130);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(299, 60);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Сохранить товар/Добавить изображение";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(106, 96);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(202, 26);
+            this.textBox5.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Категория:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(106, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(202, 26);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Цена:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 32);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(202, 26);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Название:";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 157);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(192, 32);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "Удалить категорию";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 582);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ GoodsCatalog1/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+
+namespace GoodsCatalog1
+{
+    public partial class Form1 : Form
+    {
+        string path1;
+        string path2;
+
+        string path3;
+        string path4;
+
+        XDocument doc1;
+        XDocument doc2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            path1 = @"..\..\categories.xml";
+            path2 = @"..\..\goods.xml";
+            
+            doc1 = XDocument.Load(path1);
+            doc2 = XDocument.Load(path2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            foreach (XElement c in doc1.Element("root").Elements("category"))
+                comboBox1.Items.Add(c.Attribute("name").Value);
+            comboBox1.SelectedIndex = 0;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text.Length > 0)
+            {
+               
+                doc1.Element("root").Add(new XElement("category", new XAttribute("id", "0"), new XAttribute("name", textBox1.Text)));
+                doc1.Save(path1);
+                comboBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+                MessageBox.Show("Вы добавили новую категорию", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string name = comboBox1.SelectedItem.ToString();
+            var goodsX = from p in doc2.Element("root").Elements("product")
+                         where p.Attribute("category").Value == name
+                         select p;
+            listBox1.Items.Clear();
+            foreach (XElement x in goodsX)
+                listBox1.Items.Add(String.Format("{0} - {1} uah", 
+                    x.Attribute("name").Value,
+                    x.Attribute("price").Value));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndices.Count == 0)
+                MessageBox.Show("Вы не выбрали товары для добавления", "Ошибка действия",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+                string buff = "";
+                double price1 = 0;
+                double price2 = 0;
+                int k1 = 0;
+                int k2 = 0;
+
+                foreach (int k in listBox1.SelectedIndices)
+                {
+                    buff = listBox1.Items[k].ToString();
+
+                    k1 = buff.IndexOf('-');
+                    k2 = buff.IndexOf("uah");
+
+                    price1 = Convert.ToDouble(buff.Substring(k1 + 2, k2 - k1 - 3));
+                    price2 = Convert.ToDouble(textBox2.Text);
+
+                    listBox2.Items.Add(buff);
+                    textBox2.Text = String.Format("{0}", (price1 + price2));
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndices.Count == 0)
+                MessageBox.Show("Вы не выбрали товары для удаления", "Ошибка действия",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+                string buff = "";
+                double price1 = 0;
+                double price2 = 0;
+                int k1 = 0;
+                int k2 = 0;
+                int k = 0;
+                
+                for (int i = listBox2.SelectedIndices.Count - 1; i >= 0; i--)
+                {
+                    k = listBox2.SelectedIndices[i];
+                    buff = listBox2.Items[k].ToString();
+
+                    k1 = buff.IndexOf('-');
+                    k2 = buff.IndexOf("uah");
+                    price1 = Convert.ToDouble(buff.Substring(k1 + 2, k2 - k1 - 3));
+                    price2 = Convert.ToDouble(textBox2.Text);
+
+                    textBox2.Text = String.Format("{0}", (price2 - price1));
+                    listBox2.Items.RemoveAt(k);
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string buff = listBox2.SelectedItem.ToString();
+            string[] arr = buff.Split('-');
+            string name = arr[0].Trim();
+
+            var result = from p in doc2.Element("root").Elements("product")
+                         where p.Attribute("name").Value == name
+                         select p;
+            
+            string path = "";
+            foreach (var r in result)
+                path = r.Attribute("image").Value;
+            pictureBox2.Image = new Bitmap(path);
+        }
+
+     
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string name = textBox3.Text;
+            string price = textBox4.Text;
+            string categ = textBox5.Text;
+            if (textBox3.Text != "" && textBox4.Text != "" && textBox5.Text != "")
+            {
+                MessageBox.Show("Выберете картинку для нового товара", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    path3 = ofd.FileName;
+                    path4 = @"..\..\Images\" + Path.GetFileName(path3);
+                    File.Copy(path3, path4);
+                    pictureBox2.Image = new Bitmap(path4);
+                    doc2.Element("root").Add(new XElement("product", new XAttribute("name", name), new XAttribute("price", price), new XAttribute("category", categ), new XAttribute("image", path4)));
+                    doc1.Element("root").Add(new XElement("category", new XAttribute("id", "0"), new XAttribute("name", categ)));
+                    MessageBox.Show("Вы добавили новый товар", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    doc2.Save(path2);
+                    doc1.Save(path1);
+                    textBox3.Clear();
+                    textBox4.Clear();
+                    textBox5.Clear();
+                     
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+               
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                MessageBox.Show("Вы не выбрали категорию для удаления", "Ошибка действия",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+
+                string name = comboBox1.SelectedItem.ToString();
+                var cat = from p in doc1.Element("root").Elements("category")
+                          where p.Attribute("id").Value == name || p.Attribute("name").Value == name
+                          select p;
+                
+                
+                cat.Remove();
+                doc1.Save(path1);
+
+                var cat1 = from p1 in doc2.Element("root").Elements("product")
+                           where p1.Attribute("name").Value == name && p1.Attribute("price").Value == name && p1.Attribute("category").Value == name && p1.Attribute("image").Value == name
+                           select p1;
+                
+
+                cat1.Remove();
+                doc2.Save(path2);
+                comboBox1.SelectedIndex = 0;
+                textBox1.Clear();
+                MessageBox.Show("Вы удалили категорию", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+
+
+            }
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            if (listBox1.SelectedIndex == 0)
+                MessageBox.Show("Вы не выбрали товары для удаления", "Ошибка действия",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+                string name = listBox1.SelectedItem.ToString();
+                var cat = from p in doc2.Element("root").Elements("product")
+                          where p.Attribute("name").Value == name && p.Attribute("price").Value == name && p.Attribute("category").Value == name && p.Attribute("image").Value == name
+                          select p;
+
+                
+                cat.Remove();
+                doc2.Save(path2);
+                listBox1.Items.Remove(name);
+                
+
+                
+            }
+        }
+    }
+}
